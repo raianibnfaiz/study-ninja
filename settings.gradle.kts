f213d4c5fbbf95270423a70cd51f6af3abb8e5f3
Raian Ibn Faiz <raianibnfaiz@gmail.com>
2024-02-28 17:26:24 +0600

    initialing app

---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "Ninja Study"
+include(":app")
+ 
